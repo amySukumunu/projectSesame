dc25f4efb28ccdd9626116c1ae3f345e2c270c1c
amySukumunu <amysukumunu@mmu-cyber-10.112.6.149.cyber.mmu.edu.my>
2011-11-06 12:14:28 +0800

    first-testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectSesame::Application.config.secret_token = 'fe2d645e6f3c3042fb1ab9b6d1b9ff75809a6fdd20b21dafaeac9df8403c1da7168987042bdc98b01b291cc413babfcd17d113811713b02cd8a32110bb1cb426'
